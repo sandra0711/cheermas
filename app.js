86fbeb6d95e63d8c678e6309f377c10b22d8f634
Aleksandra Troshina <aatroshina@gmail.com>
2021-06-23 18:00:53 +0300

    done

--- app.js
+++ app.js
@@ -1,42 +1,12 @@
-// Фреймворк веб-приложений.
-const express = require("express");
-const morgan = require("morgan");
-const path = require('path');
 
-const app = express();
 
-// Подключаем views(hbs)
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
 
-// Подключаем логгирование деталей запросов.
-app.use(morgan("dev"));
-
-// Обработка POST запросов.
-//распознавания входящего объекта в POST запросе в виде строк или массивов
-app.use(express.urlencoded({extended: true}));
-// распознавания входящего объекта в POST запросе как объекта JSON
-app.use(express.json());
-
-// Подключаем папку public со статическими файлами (картинки, стили и тп)
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Отображаем главную страницу с использованием шаблона "index.hbs"
-app.get('/', function(req, res) {
-  res.render('index', req.query);
-});
-const object = {
- 'RED HOT': 'H - O - T!',
- 'DO IT AGAIN': 'Go, Fight, Win',
- '2 BITS': 'Holler!',
- 'STOMP YOUR FEET': 'STOMP',
-}
 app.post('/cheers', function (req, res) {
   console.log(req.body);
   if (req.body.cheer_name in object) {
     res.render('index', {signText: object[req.body.cheer_name]})
   } else {
-    res.render('index', { signText: 'нет такой команды' })
+    res.render('index', { signText: 'Победа будет за нами!!!' })
   }
 });
 
--- server.js
+++ server.js
@@ -1,6 +1,31 @@
-const app = require("./app");
+const express = require("express");
+const app = express();
 
+app.set('view engine', 'hbs');
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 const port = process.env.PORT || 3000;
+
+const object = {
+  'RED HOT': 'H - O - T!',
+  'DO IT AGAIN': 'Go, Fight, Win',
+  '2 BITS': 'Holler!',
+  'STOMP YOUR FEET': 'STOMP',
+}
+app.get('/', function (req, res) {
+  res.render('index');
+});
+
+app.post('/cheers', function (req, res) {
+  console.log('это запрос', req.body);
+  if (req.body.cheer_name in object) {
+    res.render('index', { signText: object[req.body.cheer_name] })
+  } else {
+    res.render('index', { signText: 'Победа будет за нами!!!' })
+  }
+});
 
 app.listen(port, () => {
   console.log("Server started at http://localhost:%s/", port);
